fd61d88dab0c5727767fccf8083c69975f78ae45
cata-p <cpobleta@uc.cl>
2024-10-26 19:39:30 -0300

    tarjetas y search listo s/ conexión

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import App from './App';
 import TestComponents from './views/TestComponents';
+import YourGroups from './views/YourGroups';
 
 export default function Routing() {
     return (
         <Routes>
             <Route path="/" element={<App />} />
             <Route path="/groupcard" element={<TestComponents />} />
+            <Route path="/yourgroups" element={<YourGroups />} />
         </Routes>
     )
 }
