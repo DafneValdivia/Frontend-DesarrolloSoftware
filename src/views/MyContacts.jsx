d9abbff2c944d52be2d996268f59c7b482d7cf86
MariaBarias <mcbarias@uc.cl>
2024-11-07 13:22:11 -0300

    se agregó mis contactos y se pueden ver, pero falta funcionalidad

--- src/views/MyContacts.jsx
+++ src/views/MyContacts.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import AddContactForm from '../components/AddContactForm';
+import ContactField from "../components/ContactField";
+import "./MyContacts.css";
 
-const contactos = [
+const contactosExistentes = [
     { username: "juan", email: "juan@example.com" },
     { username: "maria", email: "maria@example.com" },
     { username: "pedro", email: "pedro@example.com" },
@@ -11,19 +13,35 @@ const contactos = [
     { username: "carlos", email: "carlos@example.com" }
 ];
 
+const misContactos = [
+    { username: "juan", email: "juan@example.com" },
+    { username: "maria", email: "maria@example.com" },
+    { username: "carlos", email: "carlos@example.com" }
+];
+
+
 export default function MyContacts() {
-    const handleGroupCreation = (groupData) => {
+    // pedir los contactos existentes al back
+
+    const handleAddContact = (groupData) => {
         console.log("Grupo creado:", groupData);
+
         // Aquí puedes manejar la lógica de envío al backend o actualización del estado global
 
     
     };
 
+    const handleContactoClicked = (contactoClicked) => {
+        console.log("Contacto Clickeado:", contactoClicked )
+        // ver si hacer algo con ese contacto o no
+    };
+
     return (
         <div>
             <Navbar />
-            <div>
-                <AddContactForm contactos={contactos} onGroupCreate={handleGroupCreation} />
+            <div className="contacts-page-container">
+                <AddContactForm contactos={contactosExistentes} onGroupCreate={handleAddContact} />
+                <ContactField contactos={misContactos} onContactoClicked={handleContactoClicked} />
             </div>
         </div>
     );
