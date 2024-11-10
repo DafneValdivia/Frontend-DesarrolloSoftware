1a7acca16b1c4361f8e6cb183e1f785a984448e0
MariaBarias <mcbarias@uc.cl>
2024-11-09 21:35:49 -0300

    finalizado los detalles, faltan endpoints

--- src/components/CreateGroupForm.jsx
+++ src/components/CreateGroupForm.jsx
@@ -14,17 +14,18 @@ const CreateGroupForm = ({ contactos, onGroupCreate }) => {
         setMemberEmail(value);
 
         if (value) {
-            const filteredSuggestions = contactos.filter((contacto) => (
+            const filteredSuggestions = contactos.filter((contacto) => 
                 (contacto.username.toLowerCase().includes(value.toLowerCase()) ||
-                 contacto.email.toLowerCase().includes(value.toLowerCase()))
-            )).filter(contacto => !members.includes(contacto.username) && !members.includes(contacto.email));
-
+                 contacto.email.toLowerCase().includes(value.toLowerCase())) &&
+                !members.some(member => member.email === contacto.email)
+        );
             setSuggestions(filteredSuggestions);
         } else {
             setSuggestions([]);
         }
     };
 
+    // Añade el contacto seleccionado al grupo y limpia las sugerencias
     const handleSelectSuggestion = (suggestion) => {
         setMemberEmail(suggestion.email);
         setSuggestions([]);
@@ -62,55 +63,55 @@ const CreateGroupForm = ({ contactos, onGroupCreate }) => {
     };
 
     return (
-        <div>
-            <div className="create-group-container">
-                <h2>CREAR NUEVO GRUPO</h2>
+        <div className="create-group-container">
+            <h2>CREAR NUEVO GRUPO</h2>
+            <input
+                type="text"
+                placeholder="Nombre del Grupo"
+                value={groupName}
+                onChange={(e) => setGroupName(e.target.value)}
+            />
+
+            <div className="member-added">
+                <h3>Integrantes Añadidos:</h3>
+                {members.map((member, index) => (
+                    <div className="show-members" key={index}>
+                        <button onClick={() => setMembers(members.filter((_, i) => i !== index))}
+                            className="X-button">
+                            X
+                        </button>
+                        <p>{member.username} <span className="suggestion-email">({member.email})</span></p>
+                    </div>
+                ))}
+            </div>
+
+            <div id="add-member-div">
                 <input
-                    type="text"
-                    placeholder="Nombre del Grupo"
-                    value={groupName}
-                    onChange={(e) => setGroupName(e.target.value)}
+                    type="email"
+                    placeholder="Usuario / Mail Integrante"
+                    value={memberEmail}
+                    onChange={handleInputChange}
                 />
-
-                <div className="member-added">
-                    <h3>Integrantes Añadidos:</h3>
-                    {members.map((member, index) => (
-                        <div className="show-members" key={index}>
-                            <button onClick={() => setMembers(members.filter((_, i) => i !== index))}
-                                className="X-button">
-                                X
-                            </button>
-                            <p>{member.username} <span className="suggestion-email">({member.email})</span></p>
-                        </div>
-                    ))}
-                </div>
-
-                <div id="add-member-div">
-                    <input
-                        type="email"
-                        placeholder="Usuario / Mail Integrante"
-                        value={memberEmail}
-                        onChange={handleInputChange}
-                    />
-                    <button className="add-member-button"><img src={AddMember} alt="Agregar" onClick={handleAddMember} /></button>
-                </div>
-
-                {suggestions.length > 0 && (
-                    <ul className="suggestions-list">
-                        {suggestions.map((suggestion, index) => (
-                            <li className="suggestion-one" key={index} onClick={() => handleSelectSuggestion(suggestion)}>
-                                <span>{suggestion.username}</span>
-                                <br />
-                                <span className="suggestion-email">({suggestion.email})</span>
-                            </li>
-                        ))}
-                    </ul>
-                )}
-
-                <button id="create-group-button" onClick={handleCreateGroup}>
-                    CREAR GRUPO
+                <button id="add-member-button" alt="Agregar" onClick={handleAddMember}>
+                    +
                 </button>
             </div>
+
+            {suggestions.length > 0 && (
+                <ul className="suggestions-list">
+                    {suggestions.map((suggestion, index) => (
+                        <li className="suggestion-one" key={index} onClick={() => handleSelectSuggestion(suggestion)}>
+                            <span>{suggestion.username}</span>
+                            <br />
+                            <span className="suggestion-email">({suggestion.email})</span>
+                        </li>
+                    ))}
+                </ul>
+            )}
+
+            <button id="create-group-button" onClick={handleCreateGroup}>
+                Crear Grupo
+            </button>
         </div>
     );
 };
